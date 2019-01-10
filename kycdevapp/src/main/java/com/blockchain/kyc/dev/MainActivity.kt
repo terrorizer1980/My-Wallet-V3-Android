9b3e2d7ee9a527a3ee3efdb9a1d0f465dc7187d6
Alan Evans <alan@blockchain.com>
2019-01-10 08:43:30 -0400

    feat(Veriff): AND-1840 Fix KycDevApp

--- kycdevapp/src/main/java/com/blockchain/kyc/dev/MainActivity.kt
+++ kycdevapp/src/main/java/com/blockchain/kyc/dev/MainActivity.kt
@@ -1,16 +1,18 @@
 package com.blockchain.kyc.dev
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import com.blockchain.kycui.navhost.KycNavHostActivity
 import com.blockchain.kycui.navhost.models.CampaignType
+import mobi.lab.veriff.sample.MainActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_kyc_dev_main)
     }
 
     fun launchKycForAirdrop(view: View) {
@@ -22,6 +24,6 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun launchVeriff(view: View) {
-
+        startActivity(Intent(this, MainActivity::class.java))
     }
 }
