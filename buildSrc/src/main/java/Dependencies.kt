ae87fb5337731bf11a624d7f2a3683c641522d1b
Chris Arriola <arriola@blockchain.com>
2019-04-25 12:55:27 -0700

    chore(Release): AND-2040 Update version code

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -6,7 +6,7 @@ object Versions {
     const val minSdk = 19
     const val targetSdk = 27
     const val compileSdk = 27
-    const val versionCode = 440
+    const val versionCode = 441
     const val versionName = "6.23.0"
     const val buildTools = "28.0.3"
 
