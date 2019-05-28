42da3b7a432d0c59d6b483cb92139410445840d4
Chris Arriola <arriola@blockchain.com>
2019-05-28 10:26:52 -0700

    chore(Release): AND-2022 Update version code

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -6,7 +6,7 @@ object Versions {
     const val minSdk = 19
     const val targetSdk = 27
     const val compileSdk = 27
-    const val versionCode = 449
+    const val versionCode = 450
     const val versionName = "6.25.0"
     const val buildTools = "28.0.3"
 
