cbc87ec4606d2066c1cc782884a0d7762041a3b2
Antonis Latas <antonis@blockchain.com>
2019-07-05 17:02:22 +0100

    feat(thepit): AND-2265 fixed tests

--- app/src/test/java/piuk/blockchain/android/ui/thepit/PitPermissionsPresenterTest.kt
+++ app/src/test/java/piuk/blockchain/android/ui/thepit/PitPermissionsPresenterTest.kt
@@ -1,11 +1,15 @@
 package piuk.blockchain.android.ui.thepit
 
 import com.blockchain.android.testutils.rxInit
+import com.blockchain.kyc.datamanagers.nabu.NabuDataManager
+import com.blockchain.nabu.NabuToken
+import com.blockchain.nabu.models.NabuOfflineTokenResponse
 import com.nhaarman.mockito_kotlin.any
 import com.nhaarman.mockito_kotlin.never
 import com.nhaarman.mockito_kotlin.whenever
 import info.blockchain.wallet.api.data.Settings
 import io.reactivex.Observable
+import io.reactivex.Single
 import org.amshove.kluent.mock
 import org.junit.Before
 import org.junit.Rule
@@ -23,12 +27,17 @@ class PitPermissionsPresenterTest {
 
     private lateinit var presenter: PitPermissionsPresenter
     private val settings: Settings = mock()
+    private val nabuDataManager: NabuDataManager = mock()
+    private val nabuToken: NabuToken = mock()
     private val settingsDataManager: SettingsDataManager = mock()
     private val view = mock<PitPermissionsView>()
 
     @Before
     fun setUp() {
-        presenter = PitPermissionsPresenter(settingsDataManager).also {
+        whenever(nabuToken.fetchNabuToken()).thenReturn(Single.just(NabuOfflineTokenResponse("", "")))
+        whenever(settingsDataManager.getSettings()).thenReturn(Observable.just(Settings()))
+
+        presenter = PitPermissionsPresenter(nabuDataManager, nabuToken, settingsDataManager).also {
             it.initView(view)
         }
     }
