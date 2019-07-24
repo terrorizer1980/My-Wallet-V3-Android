385ebd4b2a560721196febfaa21c6262870f3a88
antonis-bc <49150635+antonis-bc@users.noreply.github.com>
2019-07-24 16:36:41 +0100

    feat(pit): AND-2334 update more link (#564)

--- coreui/src/main/java/com/blockchain/ui/urllinks/Links.kt
+++ coreui/src/main/java/com/blockchain/ui/urllinks/Links.kt
@@ -30,8 +30,7 @@ const val URL_BLOCKCHAIN_GOLD_UNAVAILABLE_SUPPORT =
 const val URL_BLOCKCHAIN_KYC_SUPPORTED_COUNTRIES_LIST =
     "https://support.blockchain.com/hc/en-us/articles/360018751932"
 
-// TODO: AND-2200 - add the correct links
-const val URL_THE_PIT_LANDING_LEARN_MORE = "https://support.blockchain.com/"
+const val URL_THE_PIT_LANDING_LEARN_MORE = "https://pit.blockchain.com"
 const val URL_THE_PIT_LAUNCH_SUPPORT = "https://support.blockchain.com/"
 
 const val URL_COINIFY_POLICY = "https://coinify.com/legal"
