c832b5a91b79917293d7bb28d462184a79cfc1f7
antonis-bc <49150635+antonis-bc@users.noreply.github.com>
2019-08-21 14:41:35 +0100

    feat(bitpay): AND-2419 add headers to bitpay request (#622)

--- app/src/main/java/piuk/blockchain/android/data/api/bitpay/BitPay.kt
+++ app/src/main/java/piuk/blockchain/android/data/api/bitpay/BitPay.kt
@@ -2,13 +2,19 @@ package piuk.blockchain.android.data.api.bitpay
 
 import piuk.blockchain.android.data.api.bitpay.models.RawPaymentRequest
 import io.reactivex.Single
+import piuk.blockchain.android.BuildConfig
 import retrofit2.http.GET
 import retrofit2.http.Header
+import retrofit2.http.Headers
 import retrofit2.http.Url
 
 interface BitPay {
 
     @GET
+    @Headers(
+        "BP_PARTNER: Blockchain",
+        "BP_PARTNER_VERSION: V${BuildConfig.VERSION_NAME}"
+    )
     fun getRawPaymentRequest(
         @Url path: String,
         @Header("Accept") acceptType: String
