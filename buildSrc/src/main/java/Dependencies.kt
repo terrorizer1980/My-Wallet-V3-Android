985eda43d6b7cd3e6c367ed0230d7963ffdad862
Chris Arriola <arriola@blockchain.com>
2019-09-18 14:40:05 -0700

    chore(Release): Update version code.

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -6,7 +6,7 @@ object Versions {
     const val minSdk = 21
     const val targetSdk = 28
     const val compileSdk = 27
-    const val versionCode = 476
+    const val versionCode = 477
     const val versionName = "6.29.1"
     const val buildTools = "28.0.3"
 
