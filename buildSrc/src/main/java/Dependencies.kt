4d4d2c9c45aee9361f2694355e6d97b8d92225b3
Antonis Latas <antonis@blockchain.com>
2019-11-27 14:03:07 +0000

    chore(release): AND-2587 bump versionname and version number

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -6,8 +6,8 @@ object Versions {
     const val minSdk = 21
     const val targetSdk = 28
     const val compileSdk = 27
-    const val versionCode = 490
-    const val versionName = "6.29.6"
+    const val versionCode = 491
+    const val versionName = "6.29.7"
     const val buildTools = "28.0.3"
 
     // Build tools and languages
