1f17735d27c580996d7d293cad0403ee5ace1267
Antonis Latas <antonis@blockchain.com>
2019-12-06 16:56:01 +0000

    feat(release): AND-2611 update versionCode to 493

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -6,7 +6,7 @@ object Versions {
     const val minSdk = 21
     const val targetSdk = 28
     const val compileSdk = 28
-    const val versionCode = 492
+    const val versionCode = 493
     const val versionName = "6.30.0"
     const val buildTools = "28.0.3"
 
