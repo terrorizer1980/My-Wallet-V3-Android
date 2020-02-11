635ef6a0cc8b8cd52c7524cff4491ed383f5a14c
Antonis Latas <antonis@blockchain.com>
2020-02-11 12:16:37 +0000

    add kuc started case

--- app/src/main/java/piuk/blockchain/android/simplebuy/SimpleBuyFlowNavigator.kt
+++ app/src/main/java/piuk/blockchain/android/simplebuy/SimpleBuyFlowNavigator.kt
@@ -2,14 +2,19 @@ package piuk.blockchain.android.simplebuy
 
 import com.blockchain.swap.nabu.models.nabu.Kyc2TierState
 import com.blockchain.swap.nabu.service.TierService
-import io.reactivex.Observable
 import io.reactivex.Single
 
 class SimpleBuyFlowNavigator(private val simpleBuyModel: SimpleBuyModel, private val tierService: TierService) {
 
-    fun navigateTo(): Single<FlowScreen> = simpleBuyModel.state.flatMap {
-        if (it.currentScreen != FlowScreen.KYC) {
-            Observable.just(it.currentScreen)
+    fun navigateTo(startedFromKycResume: Boolean): Single<FlowScreen> = simpleBuyModel.state.flatMap {
+        if (startedFromKycResume || it.currentScreen == FlowScreen.KYC) {
+            tierService.tiers().toObservable().map { tier ->
+                if (tier.combinedState == Kyc2TierState.Tier2Approved) {
+                    FlowScreen.CHECKOUT
+                } else {
+                    FlowScreen.KYC
+                }
+            }
         } else {
             tierService.tiers().toObservable().map { tier ->
                 if (tier.combinedState == Kyc2TierState.Tier2Approved) {
