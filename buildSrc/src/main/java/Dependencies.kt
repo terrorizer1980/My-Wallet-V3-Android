17341091481020bf4ab400280faf7d4a420fecfa
pauls <pstevens@blockchain.com>
2020-02-19 18:16:15 +0000

    chore(release): AND-0000 update build number

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -6,8 +6,8 @@ object Versions {
     const val minSdk = 21
     const val targetSdk = 28
     const val compileSdk = 28
-    const val versionCode = 505
-    const val versionName = "6.31.2.qa"
+    const val versionCode = 509
+    const val versionName = "6.31.3"
     const val buildTools = "28.0.3"
 
     // Build tools and languages
