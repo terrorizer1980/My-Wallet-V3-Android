2f423e5367ca7425a2c33e9fd2d408fd8a349726
dserrano <dserrano@blockchain.com>
2020-05-12 15:59:37 +0100

    AND-3059 remove logs & dead code, reinstate swap

--- app/src/main/java/piuk/blockchain/android/coincore/impl/AssetActivityRepo.kt
+++ app/src/main/java/piuk/blockchain/android/coincore/impl/AssetActivityRepo.kt
@@ -3,13 +3,13 @@ package piuk.blockchain.android.coincore.impl
 import com.blockchain.swap.common.trade.MorphTrade
 import com.blockchain.swap.common.trade.MorphTradeDataManager
 import io.reactivex.Single
+import io.reactivex.rxkotlin.Singles
 import piuk.blockchain.android.coincore.ActivitySummaryItem
 import piuk.blockchain.android.coincore.ActivitySummaryList
 import piuk.blockchain.android.coincore.Coincore
 import piuk.blockchain.android.coincore.CryptoAccount
 import piuk.blockchain.android.coincore.SwapActivitySummaryItem
 import piuk.blockchain.androidcore.data.exchangerate.ExchangeRateDataManager
-import timber.log.Timber
 import java.util.Date
 
 private const val CACHE_LIFETIME = 2 * 60 * 1000
@@ -26,7 +26,6 @@ class AssetActivityRepo(
         account: CryptoAccount,
         isRefreshRequested: Boolean
     ): Single<ActivitySummaryList> {
-        Timber.e("---- fetch acc data isRefreshRequested $isRefreshRequested")
         return if (transactionCache.isNotEmpty() &&
             System.currentTimeMillis() - lastUpdatedTimestamp <= CACHE_LIFETIME &&
             !isRefreshRequested
@@ -34,14 +33,10 @@ class AssetActivityRepo(
             Single.just(
                 when (account) {
                     is AllWalletsAccount -> {
-                        Timber.e(
-                            "----- returning from cache for all wallets :${transactionCache.size}")
                         transactionCache.values.flatten()
                             .sortedByDescending { Date(it.timeStampMs) }
                     }
                     else -> {
-                        Timber.e(
-                            "----- returning from cache for account ${account.label} - ${transactionCache[account]}")
                         transactionCache[account]?.sortedByDescending {
                             Date(it.timeStampMs)
                         } ?: emptyList()
@@ -50,60 +45,32 @@ class AssetActivityRepo(
             )
         } else {
             if (account is AllWalletsAccount) {
-                Timber.e("----- getting all accounts")
-
                 account.allActivities().map { list ->
                     list.groupBy { it.account }.map {
-                        Timber.e("---- saving to cache: ${it.key} - ${it.value}")
                         transactionCache[it.key] = it.value
                     }
                     lastUpdatedTimestamp = System.currentTimeMillis()
                     list.sortedByDescending { Date(it.timeStampMs) }
                 }
             } else {
-                // fixme temp workaround for swap
-                account.activity.map { list ->
-                    transactionCache[account] = list
-                    lastUpdatedTimestamp = System.currentTimeMillis()
-                    Timber.e("----- got details for account: ${account.label} - $list")
-                    Timber.e("----- saved to cache ${transactionCache[account]}")
+                Singles.zip(
+                    fetchSwapHistory(),
+                    account.activity
+                ) { swapList, accountList ->
+                    val swaps =
+                        swapList.filter { account.cryptoCurrencies.contains(it.cryptoCurrency) }
+                    val txSet = buildTxLookupSet(swaps as List<SwapActivitySummaryItem>)
 
-                    list.sortedByDescending { Date(it.timeStampMs) }
+                    val sortedList = removeSendWhereSwapped(txSet, accountList).plus(swaps)
+                        .sortedByDescending { Date(it.timeStampMs) }
+
+                    transactionCache[account] = sortedList
+                    lastUpdatedTimestamp = System.currentTimeMillis()
+
+                    sortedList
                 }
             }
-            /* Singles.zip(
-                 fetchSwapHistory(),
-                 account.activity
-             ) { swapList, accountList ->
-                 //val swaps =
-                 //    swapList.filter { account.cryptoCurrencies.contains(it.cryptoCurrency) }
-                 //val txSet = buildTxLookupSet(swaps as List<SwapActivitySummaryItem>)
-
-                 //val sortedList = removeSendWhereSwapped(txSet, accountList).plus(swaps).sorted()
-                 transactionCache[account] = accountList
-
-                 Timber.e("----- got details for account: ${account.label} - $accountList")
-                 Timber.e("----- saved to cache ${transactionCache[account]}")
-
-                 accountList as ActivitySummaryList
-             }.toObservable()*/
         }
-
-        /*  return Observable.merge(
-              cache,
-              network)*/
-
-        /* return Singles.zip(
-             fetchSwapHistory(),
-             account.activity
-         ) { swapList, accountList ->
-             val swaps = swapList.filter { account.cryptoCurrencies.contains(it.cryptoCurrency) }
-             val txSet = buildTxLookupSet(swaps as List<SwapActivitySummaryItem>)
-
-             val sortedList = removeSendWhereSwapped(txSet, accountList).plus(swaps).sorted()
-             transactionCache[account]?.addAll(sortedList)
-             sortedList
-         }*/
     }
 
     private fun buildTxLookupSet(swapList: List<SwapActivitySummaryItem>): Set<String> =
