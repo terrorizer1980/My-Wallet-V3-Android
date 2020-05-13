6e576742f190ddc5712c8a4ed865283cd241d6a2
antonis-bc <49150635+antonis-bc@users.noreply.github.com>
2020-05-13 17:00:09 +0300

    add params for eligibility (#968)

--- nabu/src/main/java/com/blockchain/swap/nabu/datamanagers/custodialwalletimpl/LiveCustodialWalletManager.kt
+++ nabu/src/main/java/com/blockchain/swap/nabu/datamanagers/custodialwalletimpl/LiveCustodialWalletManager.kt
@@ -157,7 +157,7 @@ class LiveCustodialWalletManager(
 
     override fun isEligibleForSimpleBuy(fiatCurrency: String): Single<Boolean> =
         authenticator.authenticate {
-            nabuService.isEligibleForSimpleBuy(it, fiatCurrency)
+            nabuService.isEligibleForSimpleBuy(it, fiatCurrency, PAYMENT_METHODS)
         }.map {
             it.eligible
         }.onErrorReturn {
@@ -408,6 +408,10 @@ class LiveCustodialWalletManager(
             CardResponse.EXPIRED -> CardStatus.EXPIRED
             else -> CardStatus.UNKNOWN
         }
+
+    companion object {
+        private const val PAYMENT_METHODS = "BANK_ACCOUNT,PAYMENT_CARD"
+    }
 }
 
 private fun String.toSupportedPartner(): Partner =
