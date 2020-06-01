ebcdbed07082bd2f13d62b656a508dcf1f40eec2
dserrano <dserrano@blockchain.com>
2020-06-01 14:28:59 +0100

    AND-2988 pair -> to (2)

--- app/src/main/java/piuk/blockchain/android/ui/send/strategy/PaymentSentEvent.kt
+++ app/src/main/java/piuk/blockchain/android/ui/send/strategy/PaymentSentEvent.kt
@@ -6,7 +6,8 @@ import piuk.blockchain.androidcoreui.utils.logging.LoggingEvent
 
 fun paymentSentEvent(success: Boolean, amountSent: CryptoValue) =
     LoggingEvent("Payment Sent", mapOf(
-        Pair("Success", success),
-        Pair("Amount", amountSent.toBigDecimal().getBoundary() + " " + amountSent.currency.networkTicker),
-        Pair("Currency", amountSent.currency.networkTicker)
+        "Success" to success,
+        "Amount" to amountSent.toBigDecimal()
+            .getBoundary() + " " + amountSent.currency.networkTicker,
+        "Currency" to amountSent.currency.networkTicker
     ))
