74d7c7cfb4f35a259c0c9f60544ff6ec57474a96
dserrano <dserrano@blockchain.com>
2020-06-10 12:41:10 +0100

    AND-3214 removed named qualifier for paxAccount

--- app/src/main/java/piuk/blockchain/android/coincore/koin.kt
+++ app/src/main/java/piuk/blockchain/android/coincore/koin.kt
@@ -1,6 +1,5 @@
 package piuk.blockchain.android.coincore
 
-import com.blockchain.koin.paxAccount
 import com.blockchain.koin.payloadScopeQualifier
 import org.koin.dsl.module
 import piuk.blockchain.android.coincore.bch.BchTokens
@@ -86,7 +85,7 @@ val coincoreModule = module {
         scoped {
             PaxTokens(
                 rxBus = get(),
-                paxAccount = get(paxAccount),
+                paxAccount = get(),
                 exchangeRates = get(),
                 historicRates = get(),
                 currencyPrefs = get(),
--- common/interface/src/main/kotlin/com/blockchain/koin/Qualifiers.kt
+++ common/interface/src/main/kotlin/com/blockchain/koin/Qualifiers.kt
@@ -3,7 +3,6 @@ package com.blockchain.koin
 import org.koin.core.qualifier.StringQualifier
 import org.koin.core.qualifier.named
 
-val paxAccount = StringQualifier("pax")
 val pitFeatureFlag = StringQualifier("ff_pit_linking")
 val simpleBuyFeatureFlag = StringQualifier("ff_simple_buy")
 val cardPaymentsFeatureFlag = StringQualifier("ff_card_payments")
