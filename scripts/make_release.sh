28cf0b9fd092ffa528d146f54d1605005061ebf1
dserrano <dserrano@blockchain.com>
2020-06-15 17:03:34 +0100

    stash WIP branch creation

--- scripts/make_release.sh
+++ scripts/make_release.sh
@@ -97,17 +97,18 @@ if [ $updateConfirmation == "y" ] || [ $updateConfirmation == "Y" ]; then
   strippedUpdatedVersionName="${newVersionName%\"}"
   strippedUpdatedVersionName="${strippedUpdatedVersionName#\"}"
   releaseBranch="release/$strippedUpdatedVersionName"
-  git checkout -b $releaseBranch
+  echo "$releaseBranch"
+  #git checkout -b $releaseBranch
 
-  sed -i -e "s/$currentVersionCode/$updatedVersionCode/g" $dependenciesFilePath
-  sed -i -e "s/$currentVersionName/$newVersionName/g" $dependenciesFilePath
+  #sed -i -e "s/$currentVersionCode/$updatedVersionCode/g" $dependenciesFilePath
+  #sed -i -e "s/$currentVersionName/$newVersionName/g" $dependenciesFilePath
 
-  git add .
-  git commit -m "version bump: $newVersionName($updatedVersionCode)"
+  #git add .
+  #git commit -m "version bump: $newVersionName($updatedVersionCode)"
 
-  git tag -a "v$newVersionName($updatedVersionCode)"
-  git push --set-upstream origin $releaseBranch
-  git push origin --tags
+  #git tag -a "v$newVersionName($updatedVersionCode)"
+  #git push --set-upstream origin $releaseBranch
+  #git push origin --tags
 fi
 
 if [ $updateConfirmation != "y" ] && [ $updateConfirmation != "Y" ]; then
