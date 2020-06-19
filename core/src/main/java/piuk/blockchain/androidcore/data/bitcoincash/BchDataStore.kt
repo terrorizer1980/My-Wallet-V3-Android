1078ae1e6eda615671c31e94533bc2b087961db2
antonis-bc <49150635+antonis-bc@users.noreply.github.com>
2020-06-19 11:36:04 +0300

    fix(bch): AND-3275 fix issue with bch balances (#1063)

--- core/src/main/java/piuk/blockchain/androidcore/data/bitcoincash/BchDataStore.kt
+++ core/src/main/java/piuk/blockchain/androidcore/data/bitcoincash/BchDataStore.kt
@@ -3,6 +3,7 @@ package piuk.blockchain.androidcore.data.bitcoincash
 import info.blockchain.wallet.BitcoinCashWallet
 import info.blockchain.wallet.coin.GenericMetadataWallet
 import piuk.blockchain.androidcore.data.datastores.SimpleDataStore
+import java.math.BigInteger
 
 /**
  * A simple data store class to cache the Bitcoin cash Wallet (bitcoin chain M/44H/0H)
@@ -11,9 +12,11 @@ class BchDataStore : SimpleDataStore {
 
     var bchWallet: BitcoinCashWallet? = null
     var bchMetadata: GenericMetadataWallet? = null
+    val bchBalances = hashMapOf<String, BigInteger>()
 
     override fun clearData() {
         bchWallet = null
         bchMetadata = null
+        bchBalances.clear()
     }
 }
